b4291d446287e06140b82e19b58263fa2e661e87
Thomas Fritz <mail@fritzthomas.com>
2024-09-08 19:01:45 +0200

    cleaned up libs and helpers and outputs

--- scripts/fly_deploy.sh
+++ scripts/fly_deploy.sh
@@ -1,19 +1,3 @@
-#!/usr/bin/env bash
-# Exit on any failure
-set -eo pipefail
-
-DEBUG=${DEBUG:-}
-
-# Debug mode
-if [ -n "$DEBUG" ]; then
-  set -xu
-fi
-
-# Handle errors
-trap 'echo "Error: $? at line $LINENO" >&2' ERR
-# Cleanup before exit
-trap 'cleanup' EXIT
-
 __dirname="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source "$__dirname/lib/helper.sh"
 source "$__dirname/lib/prepare-vars.sh"
@@ -26,67 +10,62 @@ if [[ -z "$workflow_inputs" || -z "$workflow_secrets" || -z "$workflow_event" ]]
 fi
 
 prepare_globals "$workflow_inputs" "$workflow_secrets" "$workflow_event"
-if [[ $? -ne 0 ]]; then
-  error "prepare_globals failed"
+if [[ ${#WORKFLOW_INPUTS[@]} -eq 0 || ${#WORKFLOW_EVENT[@]} -eq 0 ]]; then
+  error "Something went wrong preparing the workflow inputs, or event."
   exit 1
 fi
 
 prepare_deploy_vars
-if [[ $? -ne 0 ]]; then
-  error "prepare_deploy_vars failed"
+if [[ -z "$WORKSPACE_NAME" || -z "$WORKSPACE_PATH_RELATIVE" || -z "$FLY_APP_NAME" || -z "$FLY_CONFIG_FILE_PATH" || -z "$GIT_COMMIT_SHA" || -z "$GIT_COMMIT_SHA_SHORT" ]]; then
+  error "Something went wrong preparing the necessary deploy variables."
   exit 1
 fi
 
-fly_app_create
-if [[ $? -ne 0 ]]; then
-  error "fly_app_create failed."
-  exit 1
+if ! does_fly_app_exists "$FLY_APP_NAME"; then
+  retry 5 2 "Create fly app '$FLY_APP_NAME'" flyctl apps create "$FLY_APP_NAME" --org "$FLY_ORG"
 else
-  notice "App $FLY_APP_NAME created."
+  notice "App '$FLY_APP_NAME' already exists, skipping creation."
 fi
 
-fly_secrets_import
-if [[ $? -ne 0 ]]; then
-  error "fly_secrets_import failed"
-  exit 1
-else
-  notice "Secrets imported on $FLY_APP_NAME."
-fi
-
-fly_consul_attach
-if [[ $? -ne 0 ]]; then
-  error "fly_consul_attach failed"
-  exit 1
-else
-  notice "Consul cluster attached to $FLY_APP_NAME."
-fi
-
-fly_deploy
-if [[ $? -ne 0 ]]; then
-  error "fly_deploy failed"
-  exit 1
-else
-  notice "$FLY_APP_NAME deployed successfully."
-fi
-
-group "Get the app url"
-app_url=$(fly_app_url)
-if [[ $? -ne 0 ]]; then
+if [ -n "${WORKFLOW_SECRETS[fly_secrets]}" ]; then
+  group "Set configured secrets on $FLY_APP_NAME"
+  debug "fly_secrets=${WORKFLOW_SECRETS[fly_secrets]}"
+  echo "${WORKFLOW_SECRETS[fly_secrets]}" | tr " " "\n" | flyctl secrets import --stage --app "$FLY_APP_NAME";
   group_end
-  error "fly_app_url failed"
-  exit 1
+else
+  warning "No secrets imported on $FLY_APP_NAME!";
 fi
 
-# Set the app url as an github output
-notice "app_url=$app_url"
-echo "app_url=$app_url" >> $GITHUB_OUTPUT
-group_end
+# Attach a consul cluster if requested
+if [[ "${WORKFLOW_INPUTS[fly_consul_attach]}" == "true" ]]; then
+  retry 5 2 "Attaching a consul cluster to $FLY_APP_NAME" flyctl consul attach --app "$FLY_APP_NAME"
+fi
 
+# Deploy the app to fly.io
+retry 5 2 "Deploy the app to fly.io" flyctl deploy \
+  --config "$FLY_CONFIG_FILE_PATH" \
+  --app "$FLY_APP_NAME" \
+  --build-arg "WORKSPACE_NAME=$WORKSPACE_NAME" \
+  --build-arg "WORKSPACE_PATH_RELATIVE=$WORKSPACE_PATH_RELATIVE" \
+  --build-arg "GIT_COMMIT_SHA=$GIT_COMMIT_SHA" \
+  --build-arg "GIT_COMMIT_SHA_SHORT=$GIT_COMMIT_SHA_SHORT" \
+  --remote-only \
+  --yes
+
+declare -rg FLY_APP_URL=$(get_fly_app_url "$FLY_APP_NAME")
+
+notice FLY_APP_NAME=$FLY_APP_NAME
 echo "fly_app_name=$FLY_APP_NAME" >> $GITHUB_OUTPUT
-echo "fly_app_url=$app_url" >> $GITHUB_OUTPUT
+
+notice FLY_APP_URL=$FLY_APP_URL
+echo "fly_app_url=$FLY_APP_URL" >> $GITHUB_OUTPUT
+
+notice WORKSPACE_NAME=$WORKSPACE_NAME
 echo "workspace_name=$WORKSPACE_NAME" >> $GITHUB_OUTPUT
+
+notice WORKSPACE_PATH_RELATIVE=$WORKSPACE_PATH_RELATIVE
 echo "workspace_path=$WORKSPACE_PATH_RELATIVE" >> $GITHUB_OUTPUT
 
 echo "### Deployed ${FLY_APP_NAME} :rocket:" >> $GITHUB_STEP_SUMMARY
 echo "" >> $GITHUB_STEP_SUMMARY
-echo "App URL: $app_url" >> $GITHUB_STEP_SUMMARY
+echo "Fly App URL: $FLY_APP_URL" >> $GITHUB_STEP_SUMMARY
--- scripts/lib/fly.sh
+++ scripts/lib/fly.sh
@@ -1,72 +1,14 @@
-# Check if the app already exists, if not, create it.
-# We want to have that in a separate group, thats the reason why we use status and create
-# in two separate steps
-fly_app_exists () {
-  group "Check if fly app '$FLY_APP_NAME' already exists"
-  flyctl status --app "$FLY_APP_NAME" > /dev/null 2>&1
-  local status=$?
-  group_end
-  return $status
-}
-
-fly_app_create () {
-  group "Create fly app '$FLY_APP_NAME' if it does not exist"
-  if ! fly_app_exists; then
-    retry 5 2 "Create fly app '$FLY_APP_NAME'" flyctl apps create "$FLY_APP_NAME" --org "$FLY_ORG"
-    local status=$?
-  else
-    notice "App '$FLY_APP_NAME' already exists, skipping creation."
-    local status=0
-  fi
-  group_end
-  return $status
-}
-
-
-# Import secrets if any
-fly_secrets_import () {
-  group "Set the fly secrets if any are set"
-  local status=0
-  if [ -n "${WORKFLOW_SECRETS[fly_secrets]}" ]; then
-    debug "fly_secrets=${WORKFLOW_SECRETS[fly_secrets]}"
-    echo "${WORKFLOW_SECRETS[fly_secrets]}" | tr " " "\n" | flyctl secrets import --stage --app "$FLY_APP_NAME";
-    local status=$?
-  else
-    warning "No secrets imported on $FLY_APP_NAME!";
-  fi
-  group_end
-  return $status
-}
-
-fly_consul_attach () {
-  group "Attach a consul cluster if requested"
-  local status=0
-  # Attach a consul cluster if requested
-  if [[ "${WORKFLOW_INPUTS[fly_consul_attach]}" == "true" ]]; then
-    retry 5 2 "Attaching a consul cluster" flyctl consul attach --app "$FLY_APP_NAME"
-    local status=$?
-  fi
-  group_end
-  return $status
-}
-
-# Deploy/Update the app
-fly_deploy () {
-  retry 5 2 "Deploy the app to fly.io" flyctl deploy \
-    --config "$FLY_CONFIG_FILE_PATH" \
-    --app "$FLY_APP_NAME" \
-    --build-arg "WORKSPACE_NAME=$WORKSPACE_NAME" \
-    --build-arg "WORKSPACE_PATH_RELATIVE=$WORKSPACE_PATH_RELATIVE" \
-    --build-arg "GIT_COMMIT_SHA=$GIT_COMMIT_SHA" \
-    --build-arg "GIT_COMMIT_SHA_SHORT=$GIT_COMMIT_SHA_SHORT" \
-    --remote-only \
-    --now \
-    --yes
+# Check if the app already exists
+does_fly_app_exists () {
+  local fly_app_name=$1
+  flyctl status --app "$fly_app_name" > /dev/null 2>&1
   return $?
 }
 
-fly_app_url () {
-  local app_url="https://$(flyctl status --app "$FLY_APP_NAME" --json | jq -rS '.Hostname')/"
+# Get a FQDN of the app
+get_fly_app_url () {
+  local fly_app_name=$1
+  local app_url="https://$(flyctl status --app "$fly_app_name" --json | jq -rS '.Hostname')/"
   local status=$?
   echo $app_url
   return $status
